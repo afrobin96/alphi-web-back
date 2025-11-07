673d1a6390e8377934872bd82e8856b565692843
desarrollador.web01 <desarrollador.web01@talentum.edu.co>
2025-11-06 19:28:00 -0500

    Feat: add team controller

--- src/team/team.controller.ts
+++ src/team/team.controller.ts
@@ -1,4 +1,54 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+  Put,
+  UseGuards,
+} from '@nestjs/common';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import { TeamService } from './team.service';
+import { CreateTeamDTO } from './dto/create-team.dto';
+import { UpdateTeamDTO } from './dto/update-team.dto';
 
 @Controller('team')
-export class TeamController {}
+@UseGuards(JwtAuthGuard)
+export class TeamController {
+  constructor(private readonly teamService: TeamService) {}
+
+  @Get()
+  findAll() {
+    return this.teamService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.teamService.findOne(id);
+  }
+
+  @Post()
+  create(@Body() dto: CreateTeamDTO) {
+    return this.teamService.create(dto);
+  }
+
+  @Put(':id')
+  update(@Param('id', ParseIntPipe) id: number, @Body() dto: UpdateTeamDTO) {
+    return this.teamService.update(id, dto);
+  }
+
+  @Post(':id/members/:memberId')
+  addMember(
+    @Param('id', ParseIntPipe) id: number,
+    @Param('memberId', ParseIntPipe) memberId: number,
+  ) {
+    return this.teamService.addMember(id, memberId);
+  }
+
+  @Delete(':id')
+  remove(@Param('id', ParseIntPipe) id: number) {
+    return this.teamService.remove(id);
+  }
+}
